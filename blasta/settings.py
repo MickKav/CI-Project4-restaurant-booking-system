1976d69a48303f9cd919c4ce921a76cb83274ebc
MickKav <mkavanagh187@gmail.com>
2023-10-21 14:23:40 +0000

    Adjust link in Allowed hosts in settings.py

--- blasta/settings.py
+++ blasta/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ci-project4-restaurant-booking.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['ci-project4-restaurant-booking-c617027b7c23.herokuapp.com', 'localhost']
 
 
 # Application definition
